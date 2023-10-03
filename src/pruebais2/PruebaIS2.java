9ad50c48a047698621157393dc26c68d27be49a8
ruichen <ruichen@alumni.uv.es>
2023-10-03 11:58:42 +0200

    Hola buenos dias

--- src/pruebais2/PruebaIS2.java
+++ src/pruebais2/PruebaIS2.java
@@ -10,7 +10,7 @@ public class PruebaIS2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic hevcre
+        // TODO code application logic
     }
     
 }
