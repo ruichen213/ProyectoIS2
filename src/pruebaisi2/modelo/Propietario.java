e98ea95c5ab28773081bfc5825f090f2a3289fae
msanc <msanc@DESKTOP-J5BL55O>
2023-11-07 13:41:38 +0100

    creando clase propietario

---
+++ src/pruebaisi2/modelo/Propietario.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pruebaisi2.modelo;
+
+
+/**
+ *
+ * @author msanc
+ */
+public class Propietario {
+
+}
